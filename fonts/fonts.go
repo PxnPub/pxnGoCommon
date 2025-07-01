724b1120bb347b5ea6e445a52169ce9d58e76178
lorenzop <lorenzo@poixson.com>
2025-07-01 13:24:17 -0400

    adding embedded fonts

---
+++ fonts/fonts.go
@@ -0,0 +1,28 @@
+package fonts;
+
+import _ "embed";
+
+
+//go:embed Adamina.ttf
+var Adamina []byte;
+
+//go:embed Alice.ttf
+var Alice []byte;
+
+//go:embed Brawler.ttf
+var Brawler []byte;
+
+//go:embed Brawler-Bold.ttf
+var BrawlerBold []byte;
+
+//go:embed Hack.ttf
+var Hack []byte;
+
+//go:embed Hack-Bold.ttf
+var HackBold []byte;
+
+//go:embed SpaceGrotesk.ttf
+var SpaceGrotesk []byte;
+
+//go:embed VeraSans.ttf
+var VeraSans []byte;
